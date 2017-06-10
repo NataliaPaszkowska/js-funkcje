a7aea6d74f10b1d2054138edbfadd2315acbc70d
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 14:57:29 +0200

    commit

--- js/funkcje.js
+++ js/funkcje.js
@@ -1,1 +1,26 @@
 'use strict'
+
+/*function multiply (parametr1, parametr2, parametr3) {
+	var result;
+	result = parametr1 * parametr2 * parametr3;
+	return result;
+}
+var wynik = multiply(3, 9, 10);
+
+console.log("Wynik: " + wynik);
+
+wynik = multiply(35, 2, 10);
+
+console.log("Wynik: " + wynik);*/
+
+/* funkcje anonimowe */
+
+/*var funkcja = function () {
+	return "wynik :)";
+};
+
+console.log( funkcja );
+console.log( funkcja() );
+
+
+- funkcje wywolujemy dajac nawiasy ()*/
